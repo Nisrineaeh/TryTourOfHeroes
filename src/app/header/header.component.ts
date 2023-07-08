a33be63110b8c5866054de3d0127d860afa790ba
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-07-08 17:39:28 +0200

    creation liste des hero

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Hero, Heroes } from '../hero';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title='Tour Of Heroes'
+  title='Tour Of Heroes';
+  heroes = Heroes;
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Heroes } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes = [...Heroes];
+}
